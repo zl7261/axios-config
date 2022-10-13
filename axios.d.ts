81c1769d5f87c2463846e530087f86fcee6e3d8f
zl7261 <zhenliu7261@gmail.com>
2022-10-13 15:05:33 +0800

    Update axios.d.ts

--- axios.d.ts
+++ axios.d.ts
@@ -1,5 +1,4 @@
-import { AxiosAdapter, AxiosBasicCredentials, AxiosProxyConfig, AxiosTransformer, Cancel, CancelToken } from 'axios'
-import { MessageType } from 'gleaf/types/message'
+import * as axios from 'axios';
 
 declare module 'axios' {
   // 取消axios多包的一层AxiosResponse,
@@ -9,31 +8,6 @@ declare module 'axios' {
   }
 
   export interface AxiosRequestConfig {
-    url?: string;
-    method?: string;
-    baseURL?: string;
-    transformRequest?: AxiosTransformer | AxiosTransformer[];
-    transformResponse?: AxiosTransformer | AxiosTransformer[];
-    headers?: any;
-    params?: any;
-    paramsSerializer?: (params: any) => string;
-    data?: any;
-    timeout?: number;
-    withCredentials?: boolean;
-    adapter?: AxiosAdapter;
-    auth?: AxiosBasicCredentials;
-    responseType?: string;
-    xsrfCookieName?: string;
-    xsrfHeaderName?: string;
-    onUploadProgress?: (progressEvent: any) => void;
-    onDownloadProgress?: (progressEvent: any) => void;
-    maxContentLength?: number;
-    validateStatus?: (status: number) => boolean;
-    maxRedirects?: number;
-    httpAgent?: any;
-    httpsAgent?: any;
-    proxy?: AxiosProxyConfig | false;
-    cancelToken?: CancelToken;
     /**
      * 是否隐藏请求加载动画 true是 false否
      */
@@ -50,12 +24,5 @@ declare module 'axios' {
      * 隐藏错误提示
      */
     hideWarn?: boolean
-  }
-
-  export interface CancelToken {
-    promise: Promise<Cancel>;
-    reason?: Cancel;
-
-    throwIfRequested(): void;
   }
 }
